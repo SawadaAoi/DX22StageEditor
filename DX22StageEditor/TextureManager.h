5b24da6f41a96a2b5f368947218e9a74beb25723
SawadaAoi <misokatsu2234@icloud.com>
2025-02-07 15:08:31 +0900

    草原ブロックの見た目変更

--- DX22StageEditor/TextureManager.h
+++ DX22StageEditor/TextureManager.h
@@ -33,6 +33,9 @@ public:
 		LIGHT_ICON,			// デバッグ用ライトアイコン
 		BLOCK_SIMPLE,		// シンプルなブロック
 		GROUND_GRASS,		// 草地
+		GROUND_GRASS_SIDE,	// 草地(横)
+		GROUND_GRASS_TOP,	// 草地(上)
+		GROUND_NOGRASS_SIDE,// 草地なし(横)
 		PLAYER_HP,			// プレイヤーHP
 		PLAYER_BULLET,		// プレイヤー弾
 		STAGE_GOAL,			// ゴール
@@ -47,20 +50,23 @@ public:
 	// 画像データ保存先パス
 	const std::unordered_map < TextureManager::E_TEX_KEY, std::string > MODEL_TEX_PATH
 	{
-		{TEX_KEY::TEST,				"Assets/Texture/Test.png"				},	// テスト画像
-		{TEX_KEY::TEST2,			"Assets/Texture/Star.png"				},
-		{TEX_KEY::CAMERA_ICON,		"Assets/Texture/Debug_Camera_Icon.png"	},
-		{TEX_KEY::LIGHT_ICON,		"Assets/Texture/Debug_Light_Icon.png"	},
-		{TEX_KEY::BLOCK_SIMPLE,		"Assets/Texture/Block_Simple.png"		},
-		{TEX_KEY::GROUND_GRASS,		"Assets/Texture/Ground_Grass.png"		},
-		{TEX_KEY::PLAYER_HP,		"Assets/Texture/Player_Hp.png"			},
-		{TEX_KEY::PLAYER_BULLET,	"Assets/Texture/Player_Bullet.png"		},
-		{TEX_KEY::STAGE_GOAL,		"Assets/Texture/Stage_Goal.png"			},
-		{TEX_KEY::WALL_FENCE,		"Assets/Texture/Wall_Fence.png"			},
-		{TEX_KEY::PLAYER_STARTPOS,	"Assets/Texture/Player_StartPos.png"	},
-		{TEX_KEY::TITLE_LOGO,		"Assets/Texture/Title_Logo.png"			},
-		{TEX_KEY::EFFECT_EXPLOSION, "Assets/Texture/Effect_Explosion.png"	},
-		{TEX_KEY::EFFECT_SMOKE,		"Assets/Texture/Effect_Smoke.png"		},
+		{TEX_KEY::TEST,					"Assets/Texture/Test.png"					},	// テスト画像
+		{TEX_KEY::TEST2,				"Assets/Texture/Star.png"					},
+		{TEX_KEY::CAMERA_ICON,			"Assets/Texture/Debug_Camera_Icon.png"		},
+		{TEX_KEY::LIGHT_ICON,			"Assets/Texture/Debug_Light_Icon.png"		},
+		{TEX_KEY::BLOCK_SIMPLE,			"Assets/Texture/Block_Simple.png"			},
+		{TEX_KEY::GROUND_GRASS,			"Assets/Texture/Ground_Grass.png"			},
+		{TEX_KEY::GROUND_GRASS_SIDE,	"Assets/Texture/Ground_Grass_Side.png"		},
+		{TEX_KEY::GROUND_GRASS_TOP,		"Assets/Texture/Ground_Grass_Top.png"		},
+		{TEX_KEY::GROUND_NOGRASS_SIDE,	"Assets/Texture/Ground_NoGrass_Side.png"	},
+		{TEX_KEY::PLAYER_HP,			"Assets/Texture/Player_Hp.png"				},
+		{TEX_KEY::PLAYER_BULLET,		"Assets/Texture/Player_Bullet.png"			},
+		{TEX_KEY::STAGE_GOAL,			"Assets/Texture/Stage_Goal.png"				},
+		{TEX_KEY::WALL_FENCE,			"Assets/Texture/Wall_Fence.png"				},
+		{TEX_KEY::PLAYER_STARTPOS,		"Assets/Texture/Player_StartPos.png"		},
+		{TEX_KEY::TITLE_LOGO,			"Assets/Texture/Title_Logo.png"				},
+		{TEX_KEY::EFFECT_EXPLOSION,		"Assets/Texture/Effect_Explosion.png"		},
+		{TEX_KEY::EFFECT_SMOKE,			"Assets/Texture/Effect_Smoke.png"			},
 
 	};
 
